10b6946e94a22209f4a617152e0634c4f25af659
AhmetEnesKCC <ahmetkcc2001@gmail.com>
2025-05-24 21:32:51 +0200

    FIX: db bug

--- start-app.sh
+++ start-app.sh
@@ -3,6 +3,8 @@ echo STARTING
 npm install -f
 cd server && npm install -f && npx prisma generate && cd ..
 cd app && npm install -f && cd ..
-cd client && npm install -f 
+migration=$(openssl rand -hex 16)
+echo "${migration}"
+cd client && yes | npx prisma migrate dev --name ${migration} && npm install -f 
 cd ..
 npm run start:app

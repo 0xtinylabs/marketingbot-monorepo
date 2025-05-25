ea4af1408d3e372e987d99060efc8e856402b7c1
AhmetEnesKCC <ahmetkcc2001@gmail.com>
2025-05-25 09:50:08 +0200

    Updated start script

--- start-app.sh
+++ start-app.sh
@@ -3,7 +3,7 @@ echo STARTING
 npm install -f
 migration=$(openssl rand -hex 16)
 echo "${migration}"
-# cd server && npm install -f &&  yes | npx prisma migrate dev --name ${migration} && npx prisma generate && cd ..
+cd server && npm install -f &&  yes | npx prisma migrate dev --name ${migration} && npx prisma generate && cd ..
 cd app && npm install -f && cd ..
 cd client  && npm install -f 
 cd ..

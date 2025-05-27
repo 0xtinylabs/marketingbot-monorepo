ba510ae1dea7619e74c5dde068c846a1d22ca50e
AhmetEnesKCC <ahmetkcc2001@gmail.com>
2025-05-27 14:03:36 +0200

    FIX: weth balance

--- server/src/token/token.controller.ts
+++ server/src/token/token.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Put, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseArrayPipe, Put, Query, Request, UseGuards } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { SignatureGuard } from 'src/guards/signature.guard';
 import { SIGNMESSAGES } from 'src/contants';
 import { WalletAddressedRequest } from 'src/types/common';
 import SetTokenDTO from './dto/set-token.dto';
 import { WalletRequest } from 'src/decorators/method/wallet_address.decorator';
+import TokenPredictionParamsDTO from './dto/token-prediction';
 
 @Controller('token')
 export class TokenController {
   constructor(private readonly tokenService: TokenService) { }
+
 
 
   @WalletRequest()
@@ -20,4 +22,16 @@ export class TokenController {
     const res = await this.tokenService.setUserToken(req.wallet_address, body);
     return { token: res };
   }
+
+  // @WalletRequest()
+  // @Get("/token-prediction")
+  // async getTokenPrediction(
+  //   @Request() req: WalletAddressedRequest,
+  //   @Query() params: TokenPredictionParamsDTO,
+  //   @Query("wallet_address", new ParseArrayPipe({ items: String, separator: "," })) wallet_addresses: string[]
+  // ) {
+  //   const res = this.tokenService.calculateTokenPrediction({ ...params, wallet_addresses })
+  //   return res
+  // }
+
 }
